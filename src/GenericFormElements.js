e2094256946a470e118a065b279844b19f5e3584
Mr <jpihlk@yahoo.com>
2023-08-14 23:44:23 -0400

    added two more form fields and an initial value

--- src/GenericFormElements.js
+++ src/GenericFormElements.js
@@ -6,8 +6,7 @@ import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import * as Yup from "yup";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
-import MenuItem from '@mui/material/MenuItem';
-
+import MenuItem from "@mui/material/MenuItem";
 
 // Styles
 import styles from "./Form.module.css";
@@ -17,51 +16,48 @@ import InputMask from "react-input-mask";
 import Rating from "@mui/material/Rating";
 import Card from "@mui/material/Card";
 import { ClassNames } from "@emotion/react";
-import { Container } from "@mui/material";
+import { Container, FormControl, FormLabel } from "@mui/material";
+
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 
+import Checkbox from '@mui/material/Checkbox';
+import { RestoreFromTrash } from "@mui/icons-material";
 
 // example for select
 
-
 const options = [
   {
-    value: 'option1',
-    label: 'Option 1',
+    value: "option1",
+    label: "Option 1",
   },
   {
-    value: 'option2',
-    label: 'Option 2',
+    value: "option2",
+    label: "Option 2",
   },
   {
-    value: 'option3',
-    label: 'Option 3',
+    value: "option3",
+    label: "Option 3",
   },
   {
-    value: 'option4',
-    label: 'Option 4',
+    value: "option4",
+    label: "Option 4",
   },
 ];
-
-
-
-
-
-
-
 
 const validationSchema = Yup.object({
   textField: Yup.string()
     .max(15, "Must be 15 characters or less")
     .required("Required"),
   numberField: Yup.string()
-  .max(9, 'Required / Must Be 9 Digits')
-  .min(9, 'Must Be 9 Digits')
-  .required("Required"),
-  multiline: Yup.string()
-  .required("Required"),
-  selectBox: Yup.string()
-  .required('Required')
+    .max(9, "Required / Must Be 9 Digits")
+    .min(9, "Must Be 9 Digits")
+    .required("Required"),
+  multiline: Yup.string().required("Required"),
+  selectBox: Yup.string().required("Required"),
+  radioGroup: Yup.string().required("Required"),
 });
 
 const FeedbackForm = (props) => {
@@ -69,14 +65,19 @@ const FeedbackForm = (props) => {
 
   const formik = useFormik({
     initialValues: {
-      textField: "",
-      numberField:'',
-      multiline: '',
-      selectBox:''
+      textField: "If you want an initial Value",
+      numberField: "",
+      multiline: "",
+      selectBox: "",
+      radioGroup: "",
+      checkBox:false
     },
     validationSchema: validationSchema,
-    onSubmit: (values) => {
+    onSubmit: (values,  {setSubmitting, resetForm}) => {
       alert(JSON.stringify(values, null, 2));
+      resetForm()
+      setSubmitting(false)
+    //  formik.values.textField = "Hello World!"
     },
   });
   return (
@@ -88,6 +89,8 @@ const FeedbackForm = (props) => {
         id="textField"
         name="textField"
         type="text"
+        value={formik.values.textField}
+
         label="Text Input"
         variant="filled"
         {...formik.getFieldProps("textField")}
@@ -108,11 +111,9 @@ const FeedbackForm = (props) => {
         </div>
       ) : null}
 
-
-
       {/* Example Number Field */}
       <TextField
-      fullWidth
+        fullWidth
         type="number"
         placeholder="Number Field"
         id="numberField"
@@ -128,56 +129,49 @@ const FeedbackForm = (props) => {
         </div>
       ) : null}
 
+      {/* Multiline example */}
 
-{/* Multiline example */}
+      <TextField
+        id="multiline"
+        label="Multiline"
+        multiline
+        fullWidth
+        rows={4}
+        placeholder="Multi Line Text Field"
+        style={{ marginTop: 40 }}
+        variant="filled"
+        {...formik.getFieldProps("multiline")}
+      />
 
-<TextField
-          id="multiline"
-          label="Multiline"
-          multiline
-          fullWidth
-          rows={4}
-          placeholder="Multi Line Text Field"
-          style={{ marginTop: 40 }}
-          variant="filled"
-          {...formik.getFieldProps("multiline")}
-        />
-
-
-{formik.touched.multiline && formik.errors.multiline ? (
+      {formik.touched.multiline && formik.errors.multiline ? (
         <div className="error">
           <ArrowUpwardIcon />
           {formik.errors.multiline}
         </div>
       ) : null}
 
+      {/* Select Field */}
 
+      <TextField
+        select
+        label="Select"
+        helperText="Please select your option"
+        id="selectBox"
+        label="selectBox"
+        fullWidth
+        placeholder="Select Field"
+        style={{ marginTop: 40 }}
+        variant="filled"
+        {...formik.getFieldProps("selectBox")}
+      >
+        {options.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
 
-
-
-
-{/* Select Field */}
-
-<TextField
-          select
-          label="Select"
-          helperText="Please select your option"
-          id="selectBox"
-          label="selectBox"
-          fullWidth
-          placeholder="Select Field"
-          style={{ marginTop: 40 }}
-          variant="filled"
-          {...formik.getFieldProps("selectBox")}
-        >
-          {options.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-
-{formik.touched.selectBox && formik.errors.selectBox ? (
+      {formik.touched.selectBox && formik.errors.selectBox ? (
         <div className="error">
           <ArrowUpwardIcon />
           {formik.errors.selectBox}
@@ -185,13 +179,53 @@ const FeedbackForm = (props) => {
       ) : null}
 
 
+{/* 
+Radio Group  */}
+      <FormControl>
+        <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          id="radioGroup"
+          {...formik.getFieldProps("radioGroup")}
+          name="radioGroup"
+        >
+          <FormControlLabel value="Option 1" control={<Radio />} label="Option 1" />
+          <FormControlLabel value="Option 2" control={<Radio />} label="Option 2" />
+          <FormControlLabel value="Option 3" control={<Radio />} label="Option 3" />
+        </RadioGroup>
+      </FormControl>
+
+
+      {formik.touched.radioGroup && formik.errors.radioGroup ? (
+        <div className="error">
+          <ArrowUpwardIcon />
+          {formik.errors.radioGroup}
+        </div>
+      ) : null}
+
+{/* 
+Checkbox */}
+<br />
+<FormControlLabel
+          value="top"
+          control={<Checkbox />}
+          label="Top"
+          labelPlacement="top"
+          {...formik.getFieldProps("checkBox")}
+          
+        />
 
 
 
 
 
-
-      <Button color="primary" variant="contained" fullWidth style={{marginTop: 60}} type="submit">
+      <Button
+        color="primary"
+        variant="contained"
+        fullWidth
+        style={{ marginTop: 60 }}
+        type="submit"
+      >
         Submit
       </Button>
     </form>
